210f2269d0384b4f70c3473ac5860e54354203fe
Lucas <lucasmdemello12@gmail.com>
2025-04-08 15:08:00 -0300

    deploy updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,30 @@ const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
+// Tarefa para compilar Sass
 gulp.task('sass', () => {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('dist/css'));
 });
 
+// Tarefa para processar JS
 gulp.task('js', () => {
-    return gulp.src('src/js/**/*.js')
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+  return gulp.src('src/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('watch', () => {
-    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('src/js/**/*.js', gulp.series('js'));
+// Tarefa para copiar HTML e assets
+gulp.task('copy', () => {
+  return gulp.src(['src/*.html', 'src/images/**/*'])
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.parallel('sass', 'js', 'watch'));
+// Tarefa de build para produção
+gulp.task('build', gulp.series('sass', 'js', 'copy'));
+
+// Tarefa padrão (desenvolvimento)
+gulp.task('default', gulp.series('build'));
